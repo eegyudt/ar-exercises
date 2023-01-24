a90164125dde7a55b0b48df88a691d82eafb8ae2
Eszter Egyud <eszter.egyud@telus.com>
2023-01-23 21:12:05 -0700

    Add exercise #6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,24 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Victoria", last_name: "Baker", hourly_rate: 70)
+@store1.employees.create(first_name: "Olaf", last_name: "Strongasson", hourly_rate: 40)
+@store1.employees.create(first_name: "Svenn", last_name: "Strongasson", hourly_rate: 40)
+
+@store2.employees.create(first_name: "Alba", last_name: "Manna", hourly_rate: 60)
+@store2.employees.create(first_name: "Dave", last_name: "Drum", hourly_rate: 70)
+@store2.employees.create(first_name: "Onk", last_name: "Ponk", hourly_rate: 40)
+@store2.employees.create(first_name: "Sonk", last_name: "Tonk", hourly_rate: 40)
+
+# ### Exercise 6: One-to-many association
+
+# We haven't used the Employee class (and employees table) at all yet. If you look at this table's column structure, you'll find that it has a `store_id` (integer) column. This is a column that identifies which store each employee belongs to. It points to the `id` (integer) column of their store.
+
+# Let's tell Active Record that these two tables are in fact related via the `store_id` column.
+
+# 1. Add the following code _directly_ inside the Store model (class): `has_many :employees`
+# 2. Add the following code directly inside the Employee model (class): `belongs_to :store`
+# 3. Add some data into employees. Here's an example of one (note how it differs from how you create stores): `@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)`
+# 4. Go ahead and create some more employees using the create method. You can do this by making multiple calls to create (like you have before.) No need to assign the employees to variables though. Create them through the `@store#` instance variables that you defined in previous exercises. Create a bunch under `@store1` (Burnaby) and `@store2` (Richmond). Eg: `@store1.employees.create(...)`.
